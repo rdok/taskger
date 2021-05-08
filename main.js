5a1bc6a29511443b5b9eef4becef80cb6c6f0fb4
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-08 15:29:00 +0100

    Create basic connection to mongodb.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log( "++++++++++++++++")
+console.log('================================================================================')
+const MongoClient = require('mongodb').MongoClient
+
+const {DB_HOST, DB_PORT} = process.env;
+console.log(DB_HOST)
+console.log(DB_PORT);
+
+const connectionURL = `mongodb://${DB_HOST}:${DB_PORT}`
+const databaseName = 'main'
+
+const client = new MongoClient(connectionURL, {
+  useUnifiedTopology: true
+})
+
+client.connect((err, client) => {
+  if(err) return console.error(err)
+
+  console.log('Successfull connected')
+
+  client.close();
+});
