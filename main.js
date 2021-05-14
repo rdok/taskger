c16a3f6862267937843e890699ebfd2ce3c116ac
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-14 08:50:14 +0100

    Create user & task.

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const { User } = require("./db/mongoose");
+const { ObjectId } = require("bson");
+const { User, Task } = require("./db/mongoose");
 
-const me = new User({ name: "Riz", age: 31 });
+const main = async () => {
+  // new User({ name: "Riz", age: 31, email: "bla" }).save().then((response) => {
+  //   console.log(response);
+  // });
 
-me.save().then((response) => {
-  console.log(response);
-});
+  const user = await new User({ name: "Riz", age: 31, email: "riz@mail.com" });
+  const task = await new Task({
+    name: "AlphaTask",
+    createdBy: new ObjectId(user._id),
+  });
+
+  console.log(user);
+  console.log(task);
+  // new Task({name: 'taskAlpha'})
+};
+
+main();
