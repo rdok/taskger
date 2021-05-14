7ecc65228a3fc0ad369e52b37aecd7dcc018893f
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-14 09:35:09 +0100

    Create a task with todo status.

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 const { ObjectId } = require("bson");
-const { User, Task } = require("./db/mongoose");
+const { User, Task, mongoose } = require("./db/mongoose");
 
 const main = async () => {
   // new User({ name: "Riz", age: 31, email: "bla" }).save().then((response) => {
   //   console.log(response);
   // });
 
-  const user = await new User({ name: "Riz", age: 31, email: "riz@mail.com" });
+  const user = await new User({
+    name: "Riz",
+    age: 31,
+    email: "riz@mail.com",
+  }).save();
+
   const task = await new Task({
     name: "AlphaTask",
+    status: "todo",
     createdBy: new ObjectId(user._id),
-  });
+  }).save();
 
-  console.log(user);
+  // mongoose.save();
+  // console.log(user);
   console.log(task);
   // new Task({name: 'taskAlpha'})
+
+  mongoose.connection.close();
 };
 
 main();
