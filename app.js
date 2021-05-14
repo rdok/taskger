58109e67b2e42be1fb085adab5732dd59b052fa8
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-14 16:34:36 +0100

    Configure express settings.

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+const publicDirPath = path.join(__dirname, "./public");
+const partialsDirPath = path.join(__dirname, "./views/partials");
+
+app.set("view engine", "hbs");
+
+hbs.registerPartials(partialsDirPath);
+hbs.registerHelper("date", () => new Date());
+
+app.use(express.static(publicDirPath));
+
+module.exports = app;
