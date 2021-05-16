871cc8fcb097d6fea3a5a312c5477761f67cb401
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-16 21:42:58 +0100

    Move db initilisation outside configuration.
    Fixes issue with tests not closing.

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,23 +1,6 @@
 const mongoose = require("mongoose");
 const { UserSchema, TaskSchema } = require("./schemas");
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
-const username = encodeURIComponent(DB_USER);
-const password = encodeURIComponent(DB_PASSWORD);
-const normalisedPort = DB_PORT ? `:${DB_PORT}` : "";
-const normalisedSchema =
-  process.env.NODE_ENV === "production" ? "mongodb+srv" : "mongodb";
-const query =
-  process.env.NODE_ENV === "production" ? "?retryWrites=true&w=majority" : "";
-
-const uri = `${normalisedSchema}://${username}:${password}@${DB_HOST}${normalisedPort}/${DB_NAME}${query}`;
-
-mongoose.connect(uri, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-});
-
 const User = mongoose.model("User", UserSchema);
 const Task = mongoose.model("Task", TaskSchema);
 
