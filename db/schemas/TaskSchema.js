5babe97ee87805c8f513ec4a3245b4562c319b60
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-16 16:35:40 +0100

    Temporarily disable schema validation.

--- db/schemas/TaskSchema.js
+++ db/schemas/TaskSchema.js
@@ -4,20 +4,20 @@ const TaskSchema = {
     required: true,
     trim: true,
   },
-  status: {
-    type: String,
-    required: true,
-    enum: ["completed", "todo", "doing"],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  createdBy: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+  // status: {
+  //   type: String,
+  //   required: true,
+  //   enum: ["completed", "todo", "doing"],
+  // },
+  // description: {
+  //   type: String,
+  //   trim: true,
+  // },
+  // createdBy: {
+  //   type: String,
+  //   required: true,
+  //   trim: true,
+  // },
 };
 
 module.exports = { TaskSchema };
