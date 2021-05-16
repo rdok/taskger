ce8f68359ceaf5d5e07e8b44bc7e0ec6119be84c
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-16 16:34:35 +0100

    Assert creates, updates, deletes & fetches tasks.

--- __tests__/tasks.test.js
+++ __tests__/tasks.test.js
@@ -1,21 +1,57 @@
 const { Task } = require("../db/mongoose");
+const request = require("supertest");
+const { makeTask } = require("../jest/taskMaker");
 
 jest.mock("../db/mongoose");
 
-const request = require("supertest");
 const app = require("../app");
-const { makeTask } = require("../jest/taskMaker");
 
-it("may fetch tasks", async () => {
-  // given i have two tasks in the mongo db
-  // const tasks = makeTasks(2);
-  const tasks = [makeTask()];
-  Task.find.mockResolvedValue(tasks);
-  // mockDbTaskQuery(tasks);
+describe("CRUD Tasks", () => {
+  it("fetches tasks", async () => {
+    const tasks = [makeTask()];
+    Task.find.mockResolvedValue(tasks);
 
-  await request(app)
-    .get("/api/tasks")
-    .expect("Content-Type", /json/)
-    .expect(200)
-    .expect({ data: tasks });
+    const response = request(app).get("/api/tasks");
+
+    await response.expect(200).expect({ data: tasks });
+    expect(Task.find).toHaveBeenCalled();
+  });
+
+  it("stores a task", async () => {
+    const task = makeTask();
+    const taskToCreate = { ...task, _id: "task-to-create-id" };
+    const taskModel = new Task();
+    taskModel.save.mockResolvedValue(taskToCreate);
+
+    const response = request(app).post("/api/tasks").send(task);
+
+    await response.expect(201, taskToCreate);
+    expect(taskModel.save).toHaveBeenCalled();
+  });
+
+  it("updates a task", async () => {
+    const task = { ...makeTask(), _id: "task-to-update-uuid" };
+    const updatedTask = { ...task, name: "updated-value" };
+    Task.updateOne.mockResolvedValue(updatedTask);
+
+    const response = request(app)
+      .patch(`/api/tasks/${task._id}`)
+      .send(updatedTask);
+
+    await response.expect(200, updatedTask);
+    expect(Task.updateOne).toHaveBeenCalledWith(
+      { _id: task._id },
+      { ...updatedTask }
+    );
+  });
+
+  it("deletes a task", async () => {
+    const task = { ...makeTask(), _id: "task-to-delete-uuid" };
+    Task.deleteOne.mockResolvedValue({ deleteCount: 1 });
+
+    const response = request(app).delete(`/api/tasks/${task._id}`);
+
+    await response.expect(200, { deleteCount: 1 });
+    expect(Task.deleteOne).toHaveBeenCalledWith({ _id: task._id });
+  });
 });
