a9adfc46a7365eb06ea4f4b5b485229a916a34a5
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-30 18:15:50 +0100

    Re-design: move db connect to it's own file.

---
+++ db/connect.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const username = encodeURIComponent(DB_USER);
+const password = encodeURIComponent(DB_PASSWORD);
+const normalisedPort = DB_PORT ? `:${DB_PORT}` : "";
+
+// PROD: database need to be manually created. See `DB_NAME` on heroku config var to find it out.
+// PROD Update1: also it takes some minutes in order to start communication with Cloud MongoDB.
+const normalisedSchema =
+  process.env.NODE_ENV === "production" ? "mongodb+srv" : "mongodb";
+const query =
+  process.env.NODE_ENV === "production" ? "?retryWrites=true&w=majority" : "";
+const uri = `${normalisedSchema}://${username}:${password}@${DB_HOST}${normalisedPort}/${DB_NAME}${query}`;
+
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
--- db/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const { UserSchema, TaskSchema } = require("./schemas");
-
-const User = mongoose.model("User", UserSchema);
-const Task = mongoose.model("Task", TaskSchema);
-
-module.exports = { mongoose, User, Task };
