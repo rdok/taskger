95d051527034dcdee95d25d06a0a8effc8a8fdb2
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-13 18:01:55 +0100

    Set default error message.

--- errors/NotFoundError.js
+++ errors/NotFoundError.js
@@ -1,7 +1,11 @@
 class NotFoundError extends Error {
   constructor(...params) {
     // Pass remaining arguments (including vendor specific ones) to parent constructor
-    super(...params);
+    if (params.length === 0) {
+      super("The specified resource does not exist.");
+    } else {
+      super(...params);
+    }
 
     // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
